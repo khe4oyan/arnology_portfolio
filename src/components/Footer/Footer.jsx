9fc32a5122f1ebc1b85eebc80b58846e3bfbf934
khe4oyan <khe4oyan@gmail.com>
2025-02-05 14:38:13 +0400

    footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import classes from './styles.module.css';
 export default function Footer() {
   return (
     <div className={classes.root}>
-      (Footer)
+      <p>by <a href="https://khechoyan.xyz/" target='_blank'>khechoyan.xyz</a></p>
     </div>
   )
 }
