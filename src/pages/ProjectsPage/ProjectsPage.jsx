a799a67d9b84118ca09a12872f5b00058267276b
khe4oyan <khe4oyan@gmail.com>
2025-02-05 15:02:39 +0400

    .

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,10 +1,19 @@
+// data
+import projects from '../../data/projects';
+
 // styles
 import classes from './styles.module.css';
 
 export default function ProjectsPage() {
   return (
-    <div className={classes.root}>
-      (ProjectsPage)
+    <div className={`container ${classes.root}`}>
+      {projects.map((projectData, ind) =>
+        <div key={ind} className={classes.project}>
+          <p className={classes.title}>{projectData.title}</p>
+          <p className={classes.whatIs}>{projectData.whatIs}</p>
+          <a href={projectData.link}>demo</a>
+        </div>
+      )}
     </div>
   )
 }
