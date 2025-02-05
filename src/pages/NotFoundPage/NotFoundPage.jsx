b79099b994018cc503f78de05023901791de4a7a
khe4oyan <khe4oyan@gmail.com>
2025-02-05 14:09:42 +0400

    not found page complete

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,18 @@
+// libs
+import { useNavigate } from 'react-router-dom';
+
 // styles
 import classes from './styles.module.css';
 
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.root}>
-      (NotFoundPage)
+      <div>
+        <p><span>404</span> | page not found</p>
+        <button onClick={() => navigate(-1)}>back</button>
+      </div>
     </div>
   )
 }
