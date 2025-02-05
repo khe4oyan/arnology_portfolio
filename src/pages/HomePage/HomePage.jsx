591eb4379a7374934a94301138715c61e6d69e5f
khe4oyan <khe4oyan@gmail.com>
2025-02-05 14:10:18 +0400

    Home Page complete

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,32 @@
+// libs
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+// components
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+// routes
+import ROUTES from '../../utils/routes';
+
 // styles
 import classes from './styles.module.css';
 
 export default function HomePage() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname.length <= 1) {
+      navigate(ROUTES.ABOUT);
+    }
+  }, []);
+
   return (
     <div className={classes.root}>
-      (HomePage)
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
