f3e1ef28531342c6da1d00a7c8d79712b9d03702
khe4oyan <khe4oyan@gmail.com>
2025-02-05 14:34:29 +0400

    Header complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
+// libs
+import { Link } from 'react-router-dom';
+
+// routes
+import ROUTES from '../../utils/routes';
+
 // styles
 import classes from './styles.module.css';
 
 export default function Header() {
+  const links = [
+    {title: "About", to: ROUTES.ABOUT},
+    {title: "Projets", to: ROUTES.PROJECTS},
+    {title: "Contact", to: ROUTES.CONTACT},
+  ];
+
   return (
     <div className={classes.root}>
-      (Header)
+      <div className={`container ${classes.container}`}>
+        <div>
+          <h1>Portfolio</h1>
+        </div>
+        <div className={classes.links}>
+          {links.map((link, ind) =>
+            <Link className={classes.link} to={link.to} key={ind}>{link.title}</Link>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
