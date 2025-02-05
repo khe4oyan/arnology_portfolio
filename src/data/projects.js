dc6b1abc536ec08dd0b83dc4e97d822822230df5
khe4oyan <khe4oyan@gmail.com>
2025-02-05 14:55:50 +0400

    .

---
+++ src/data/projects.js
@@ -0,0 +1,54 @@
+const projects = [
+  {
+    link: "https://github.com/khe4oyan/learn_programming_site",
+    title: "Learn Programming",
+    whatIs: "A site for training various skills with the ability to admin panel with a convenient editor of a new post",
+  },
+  {
+    link: "https://khe4oyan.github.io/react_filmHub/",
+    title: 'FilmHub',
+    whatIs: 'Films list with ganres',
+  },
+  {
+    link: "https://khe4oyan.github.io/react-shop/",
+    title: 'Buy Craft Sell',
+    whatIs: 'Game where you can buy cases, craft items and sell',
+  },
+  {
+    link: "https://github.com/khe4oyan/discord_bot",
+    title: "Discord BOT | Барахолка",
+    whatIs: "This is a discord bot that adds a mini-game to chats with interesting mechanics of seasonal events, cases and items!",
+  },
+  {
+    link: "https://github.com/khe4oyan/FastCssCopy-vscode",
+    title: 'Fast CSS Copy',
+    whatIs: 'Is a extention for Visual Studio Code for Speed up development by copying styles from HTML to CSS with a single keyboard shortcut',
+  },
+  {
+    link: "https://github.com/khe4oyan/CleanUpReact-vscode",
+    title: 'Clean Up React',
+    whatIs: 'Clean-Up React is a VS Code extension that automatically removes unnecessary files created when initializing a React project and cleans up the remaining files from unnecessary information.',
+  },
+  {
+    link: "https://github.com/khe4oyan/fomponent-vscode",
+    title: 'Fomponent',
+    whatIs: 'Fomponent is a extention for Visual Studio Code for instant creation of React.JS components in your project',
+  },
+  {
+    link: "https://github.com/khe4oyan/picsart_academy_project_interpretator",
+    title: 'Interpreter',
+    whatIs: 'An interpreter written in C++ that compiles a fictional programming language.',
+  },
+  {
+    link: "https://github.com/khe4oyan/photobomber",
+    title: 'Photobomber | Photo Storage',
+    whatIs: 'Storage of photos in separate albums, sorted by date of their addition',
+  },
+  {
+    link: "https://github.com/khe4oyan/svg_editor",
+    title: 'SVG Editor',
+    whatIs: 'Client side SVG editor',
+  },
+];
+
+export default projects;
