c4ddc60c595fd50a5842f24eed80d3683770f7bb
khe4oyan <khe4oyan@gmail.com>
2025-02-05 13:17:11 +0400

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// libs
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// main entry point component
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
